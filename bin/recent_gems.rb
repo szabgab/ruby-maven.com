f85080c35e90f6c4ef2d9acf93a9aa63a17207eb
Gabor Szabo <gabor@szabgab.com>
2015-11-25 22:49:37 +0200

    stop fetching from the travis-api; put code in main() function

--- bin/recent_gems.rb
+++ bin/recent_gems.rb
@@ -2,7 +2,93 @@ require 'open-uri'
 require 'json'
 require 'pp'
 
-url = 'https://rubygems.org/api/v1/activity/just_updated.json'
+
+def main()
+    url = 'https://rubygems.org/api/v1/activity/just_updated.json'
+    response = get(url)
+    gems = JSON.parse(response)
+    #pp gems
+    
+    recent = []
+    
+    gems.each do |g|
+        #puts g['name']
+        #puts g['homepage_uri']
+        item = {
+           'name' => g['name'],
+        }
+        if (not g['homepage_uri'])
+            item['error'] = 'homepage_url missing'
+            recent.push(item)
+            next
+        end
+        item['homepage_uri'] = g['homepage_uri']
+        m = %r{^https?://github.com/(.*)}.match(item['homepage_uri'])
+        if (not m)
+            item['error'] = 'homepage_uri is not to github'
+            recent.push(item)
+            next
+        end
+    
+        #puts m[1]
+    
+        # check in cache?
+    
+        # check Github for a Travis-CI file
+        # and what if they don't use master as their primary branch?
+        travis_yml_url = 'https://raw.githubusercontent.com/' + m[1] + '/master/.travis.yml'
+        begin
+           travis_yml = get(travis_yml_url)
+        rescue
+           #puts 'No travis.yml fund'
+        end
+        if (not travis_yml)
+            item['error'] = 'no travis.yml found in git repository'
+    		recent.push(item)
+    		next
+        end
+    
+        #travis_url = 'https://api.travis-ci.org/repos/' + m[1] + '/builds';
+    	## remove / from m[1]
+    	#begin
+        #	res = get( travis_url, {'Accept' => 'application/vnd.travis-ci.2+json'})
+    	#rescue
+    	#	item['error'] = "Failed to fetch data from travis API: '#{travis_url}'"
+    	#	recent.push(item)
+    	#	next
+    	#end
+        ##puts res
+        ##puts '-----'
+        #data = JSON.parse(res)
+        ##pp data
+        #if (not data or not data['builds'] or data['builds'].length == 0)
+        #    # was this a JSON parse error or was this the exception that still set travis_yml to some value?
+        #    item['error'] = 'Could not find builds in data received from travis-ci.org'
+        #    recent.push(item)
+        #    next
+        #end
+    
+        #begin
+        #    item['travis_status'] = get_travis_status( data['builds'] )
+        #rescue
+        #    puts travis_yml
+        #    puts '---'
+        #    pp data
+        #    exit
+        #end
+        recent.push(item)
+        break if recent.length > 5
+    end
+    
+    #pp recent
+    
+    # save to json file
+    # another piece of code to generate html
+    f = File.new('recent.json', 'w')
+    f.write(JSON.generate(recent))
+    f.close
+end
+
 
 def get(url, params = {})
     #params ||= {}
@@ -28,87 +114,4 @@ def get_travis_status(builds)
     return 'unknown'
 end
 
-
-response = get(url)
-gems = JSON.parse(response)
-#pp gems
-
-recent = []
-
-gems.each do |g|
-    #puts g['name']
-    #puts g['homepage_uri']
-    item = {
-       'name' => g['name'],
-    }
-    if (not g['homepage_uri'])
-        item['error'] = 'homepage_url missing'
-        recent.push(item)
-        next
-    end
-    item['homepage_uri'] = g['homepage_uri']
-    m = %r{^https?://github.com/(.*)}.match(item['homepage_uri'])
-    if (not m)
-        item['error'] = 'homepage_uri is not to github'
-        recent.push(item)
-        next
-    end
-
-    #puts m[1]
-
-    # check in cache?
-
-    # check Github for a Travis-CI file
-    # and what if they don't use master as their primary branch?
-    travis_yml_url = 'https://raw.githubusercontent.com/' + m[1] + '/master/.travis.yml'
-    begin
-       travis_yml = get(travis_yml_url)
-    rescue
-       #puts 'No travis.yml fund'
-    end
-    if (not travis_yml)
-        item['error'] = 'no travis.yml found in git repository'
-		recent.push(item)
-		next
-    end
-
-    travis_url = 'https://api.travis-ci.org/repos/' + m[1] + '/builds';
-	# remove / from m[1]
-	begin
-    	res = get( travis_url, {'Accept' => 'application/vnd.travis-ci.2+json'})
-	rescue
-		item['error'] = "Failed to fetch data from travis API: '#{travis_url}'"
-		recent.push(item)
-		next
-	end
-    #puts res
-    #puts '-----'
-    data = JSON.parse(res)
-    #pp data
-    if (not data or not data['builds'] or data['builds'].length == 0)
-        # was this a JSON parse error or was this the exception that still set travis_yml to some value?
-        item['error'] = 'Could not find builds in data received from travis-ci.org'
-        recent.push(item)
-        next
-    end
-
-    begin
-        item['travis_status'] = get_travis_status( data['builds'] )
-    rescue
-        puts travis_yml
-        puts '---'
-        pp data
-        exit
-    end
-    recent.push(item)
-    #break if recent.length > 5
-end
-
-#pp recent
-
-# save to json file
-# another piece of code to generate html
-f = File.new('recent.json', 'w')
-f.write(JSON.generate(recent))
-f.close
-
+main()
